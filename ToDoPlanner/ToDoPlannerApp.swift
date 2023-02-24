3da3df93be5bbc87a646223fd4fe9c05cdc7e970
jjensen-ai <jordon.jensen@live.ca>
2023-02-24 13:25:46 -0500

    Initial Commit

---
+++ ToDoPlanner/ToDoPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoPlannerApp.swift
+//  ToDoPlanner
+//
+//  Created by Jordon Jensen on 2023-02-24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
